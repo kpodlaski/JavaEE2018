40edb9672b84d7b6fbefb49d26582141a59aac7a
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2019-01-16 14:24:55 +0100

    16.01.get i post

--- sroda/rest_client/html/main.js
+++ sroda/rest_client/html/main.js
@@ -2,12 +2,13 @@
  * Created by Krzysztof Podlaski on 09.01.2019.
  */
 
-$.ajax({
-    url: "http://localhost:8080/people/",
-    }).done(function( data ) {
+function loadData() {
+    $.ajax({
+        url: "http://localhost:8080/people",
+    }).done(function (data) {
         console.log("Mam polaczenie");
         console.log(JSON.stringify(data));
-        data._embedded.people.forEach(function(item) {
+        data._embedded.people.forEach(function (item) {
             var row = document.createElement("tr");
             var col = document.createElement("td");
             col.innerText
@@ -23,3 +24,28 @@ $.ajax({
 
         //alert(data._embedded.people[0].firstName);
     });
+}
+
+loadData();
+
+submit_btn = document.getElementById("submit_btn");
+submit_btn.addEventListener("click",function(event){
+   event.preventDefault();
+   data = {};
+   data.firstName = document.getElementById("imie").value;
+   data.lastName = document.getElementById("nazw").value;
+   console.log(data);
+    $.ajax({
+        url: "http://localhost:8080/people",
+        method:"POST",
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify(data),
+    }).done(function( data ) {
+        console.log("Udało się wysłać");
+        loadData();
+    }).fail(function(msg) {
+        console.log("ERRORRR");
+        console.log(msg);
+    })
+});
